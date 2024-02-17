30b4b4cd706632f5670bab265e4e139e80273189
Justin <justin.nikles1@gmail.com>
2024-02-17 17:17:41 -0500

    modify to surpass

--- simulation.py
+++ simulation.py
@@ -3,10 +3,11 @@ def get_probability(blocks_behind: int, attacker_resources: float, sum=0, index=
     # y is lambda
     honest_resources = 1 - attacker_resources
     y = blocks_behind * (attacker_resources / honest_resources)
+    blocks_to_surpass = blocks_behind + 1
     poisson_density = (y**index * math.exp(-y))/ math.factorial(index)
-    catchup_probability = 1 - ((attacker_resources / honest_resources)**(blocks_behind - index))
+    catchup_probability = 1 - ((attacker_resources / honest_resources)**(blocks_to_surpass - index))
     new_sum = sum + (poisson_density * catchup_probability)
-    if index < blocks_behind:
+    if index < blocks_to_surpass:
         return get_probability(blocks_behind, attacker_resources, sum=new_sum, index=index + 1)
     else:
         return(1 - new_sum)
@@ -17,6 +18,5 @@ z_inc = 1
 print(f"q={q}")
 for i in range(z_nums):
     z = i * z_inc
-    #surpass = z + 1
     probability = get_probability(z, q)
     print(f"z={z} P={probability}")
